b3bede714e03a93843779d24cf5aee67b015cc6e
Danny Summerlin <danny@summerlin.co>
2025-06-11 16:54:13 +0100

    adjusting for nonsense

--- aura/portAll/portAllController.js
+++ aura/portAll/portAllController.js
@@ -1,13 +1,18 @@
 ({
 	init: function(c,e,h) {
-		if(c.get('v.raw') {
+        let content = '<details '+ (c.get('v.hide') ? '' : 'open="true"') +">\n"
+        content += '<summary>' + c.get('v.title') + '</summary>'
+		if(c.get('v.iframe')) {
+            content += '<iframe src="' + c.get('v.url') + '" width="100%" frameborder="0" height="' + (c.get('v.height') ? c.get('v.height') : '200')  + '"></iframe>'
+        } else {
 			try {
 				const url = c.get('v.url')
 				fetch(url).then((c)=>{
 					// space for more processing here
-					c.set('rawContent', c.html())
+					c.set('v.rawContent', c.html())
 				})
 			} catch(e) { console.log(e) }
-		}
+ 		}
+        c.set('v.rawContent', content + '</details>')
 	}
 })
